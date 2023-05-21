774fc439640457816b0854d8a5c70afe4831beb3
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-05-21 11:58:05 +0800

    feat(react): prettier rules

--- packages/react/index.js
+++ packages/react/index.js
@@ -15,5 +15,20 @@ module.exports = {
     'jsx-quotes': ['error', 'prefer-double'],
     'react/react-in-jsx-scope': 'off',
     'react/self-closing-comp': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        bracketSpacing: true,
+        singleQuote: true,
+        arrowParens: 'always',
+        bracketSameLine: false,
+        jsxSingleQuote: true,
+        trailingComma: 'none',
+        tabWidth: 2,
+        useTabs: false,
+        endOfLine: 'lf',
+      },
+    ],
   },
 };
